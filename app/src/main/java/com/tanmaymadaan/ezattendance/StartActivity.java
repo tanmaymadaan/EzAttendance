94ad62d36111016305b29d9aa97022d09df9a56b
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-07 02:38:05 +0530

    Adding missing files

--- app/src/main/java/com/tanmaymadaan/ezattendance/StartActivity.java
+++ app/src/main/java/com/tanmaymadaan/ezattendance/StartActivity.java
@@ -2,13 +2,36 @@ package com.tanmaymadaan.ezattendance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    private Button loginButton, registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        loginButton = findViewById(R.id.start_login);
+        registerButton = findViewById(R.id.start_register);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
